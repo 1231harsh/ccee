dbef8c4fccfc3be757911486bc041ca2f27b601b
1231harsh <1206harsh@gmail.com>
2026-04-22 23:18:48 +0530

    User entity updated

--- Server/src/main/java/com/exam/ccee/entity/User.java
+++ Server/src/main/java/com/exam/ccee/entity/User.java
@@ -1,9 +1,6 @@
 package com.exam.ccee.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -12,7 +9,7 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
